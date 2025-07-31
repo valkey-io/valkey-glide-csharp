5ad967c9d22194fbaad051d79e892dfe30422b4b
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-07-31 10:45:56 -0700

    Initial commit - copying C# code from main repo @ 280b91054
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

---
+++ sources/Valkey.Glide/BaseClient.ConnectionManagementCommands.cs
@@ -0,0 +1,10 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+using Valkey.Glide.Commands;
+
+namespace Valkey.Glide;
+
+public abstract partial class BaseClient : IConnectionManagementCommands
+{
+    // Connection management commands will be put here
+}
