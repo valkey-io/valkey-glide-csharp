d2efdea2357c19cf1e50c4e428000b91178ccd87
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-08-08 10:26:17 -0700

    Update FFI delegates.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

---
+++ sources/Valkey.Glide/Internals/FFI.methods.cs
@@ -0,0 +1,47 @@
+﻿// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+// check https://stackoverflow.com/a/77455034 if you're getting analyzer error (using is unnecessary)
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Valkey.Glide.Internals;
+
+internal partial class FFI
+{
+#if NET8_0_OR_GREATER
+    [LibraryImport("libglide_rs", EntryPoint = "command")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static partial void CommandFfi(IntPtr client, ulong index, IntPtr cmdInfo, IntPtr routeInfo);
+
+    [LibraryImport("libglide_rs", EntryPoint = "batch")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static partial void BatchFfi(IntPtr client, ulong index, IntPtr batch, [MarshalAs(UnmanagedType.U1)] bool raiseOnError, IntPtr opts);
+
+    [LibraryImport("libglide_rs", EntryPoint = "free_response")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static partial void FreeResponse(IntPtr response);
+
+    [LibraryImport("libglide_rs", EntryPoint = "create_client")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static partial void CreateClientFfi(IntPtr config, IntPtr successCallback, IntPtr failureCallback);
+
+    [LibraryImport("libglide_rs", EntryPoint = "close_client")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static partial void CloseClientFfi(IntPtr client);
+#else
+    [DllImport("libglide_rs", CallingConvention = CallingConvention.Cdecl, EntryPoint = "command")]
+    public static extern void CommandFfi(IntPtr client, ulong index, IntPtr cmdInfo, IntPtr routeInfo);
+
+    [DllImport("libglide_rs", CallingConvention = CallingConvention.Cdecl, EntryPoint = "batch")]
+    public static extern void BatchFfi(IntPtr client, ulong index, IntPtr batch, [MarshalAs(UnmanagedType.U1)] bool raiseOnError, IntPtr opts);
+
+    [DllImport("libglide_rs", CallingConvention = CallingConvention.Cdecl, EntryPoint = "free_response")]
+    public static extern void FreeResponse(IntPtr response);
+
+    [DllImport("libglide_rs", CallingConvention = CallingConvention.Cdecl, EntryPoint = "create_client")]
+    public static extern void CreateClientFfi(IntPtr config, IntPtr successCallback, IntPtr failureCallback);
+
+    [DllImport("libglide_rs", CallingConvention = CallingConvention.Cdecl, EntryPoint = "close_client")]
+    public static extern void CloseClientFfi(IntPtr client);
+#endif
+}
