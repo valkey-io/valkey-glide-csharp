0366395d07a2ce0a7ae9f020b2b065267d470d28
Alex Rehnby-Martin <alex.rehnby-martin@improving.com>
2025-09-09 09:40:18 -0700

    Fix doc
    Signed-off-by: Alex Rehnby-Martin <alex.rehnby-martin@improving.com>

--- sources/Valkey.Glide/Pipeline/IBatchServerManagementCommands.cs
+++ sources/Valkey.Glide/Pipeline/IBatchServerManagementCommands.cs
@@ -4,73 +4,45 @@ using Valkey.Glide.Commands;
 
 namespace Valkey.Glide.Pipeline;
 
-/// <summary>
-/// Supports commands for the "Server Management" group for batch operations.
-/// </summary>
 internal interface IBatchServerManagementCommands
 {
-    /// <summary>
-    /// Get all configuration parameters matching the specified pattern.
-    /// </summary>
-    /// <param name="pattern">The pattern of config values to get.</param>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.ConfigGetAsync(ValkeyValue, CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.ConfigGetAsync(ValkeyValue, CommandFlags)" /></returns>
     IBatch ConfigGetAsync(ValkeyValue pattern = default);
 
-    /// <summary>
-    /// Resets the statistics reported by Valkey using the INFO command.
-    /// </summary>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.ConfigResetStatisticsAsync(CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.ConfigResetStatisticsAsync(CommandFlags)" /></returns>
     IBatch ConfigResetStatisticsAsync();
 
-    /// <summary>
-    /// The CONFIG REWRITE command rewrites the valkey.conf file.
-    /// </summary>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.ConfigRewriteAsync(CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.ConfigRewriteAsync(CommandFlags)" /></returns>
     IBatch ConfigRewriteAsync();
 
-    /// <summary>
-    /// The CONFIG SET command is used to reconfigure the server at runtime.
-    /// </summary>
-    /// <param name="setting">The setting name.</param>
-    /// <param name="value">The new setting value.</param>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.ConfigSetAsync(ValkeyValue, ValkeyValue, CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.ConfigSetAsync(ValkeyValue, ValkeyValue, CommandFlags)" /></returns>
     IBatch ConfigSetAsync(ValkeyValue setting, ValkeyValue value);
 
-    /// <summary>
-    /// Returns the number of keys in the currently-selected database.
-    /// </summary>
-    /// <param name="database">The database ID.</param>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.DatabaseSizeAsync(int, CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.DatabaseSizeAsync(int, CommandFlags)" /></returns>
     IBatch DatabaseSizeAsync(int database = -1);
 
-    /// <summary>
-    /// Delete all the keys of all databases on the server.
-    /// </summary>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.FlushAllDatabasesAsync(CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.FlushAllDatabasesAsync(CommandFlags)" /></returns>
     IBatch FlushAllDatabasesAsync();
 
-    /// <summary>
-    /// Delete all the keys of the database.
-    /// </summary>
-    /// <param name="database">The database ID.</param>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.FlushDatabaseAsync(int, CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.FlushDatabaseAsync(int, CommandFlags)" /></returns>
     IBatch FlushDatabaseAsync(int database = -1);
 
-    /// <summary>
-    /// Return the time of the last DB save executed with success.
-    /// </summary>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.LastSaveAsync(CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.LastSaveAsync(CommandFlags)" /></returns>
     IBatch LastSaveAsync();
 
-    /// <summary>
-    /// The TIME command returns the current server time in UTC format.
-    /// </summary>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.TimeAsync(CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.TimeAsync(CommandFlags)" /></returns>
     IBatch TimeAsync();
 
-    /// <summary>
-    /// The LOLWUT command displays the Valkey version and a piece of generative computer art.
-    /// </summary>
-    /// <returns>A task representing the batch operation.</returns>
+    /// <inheritdoc cref="IServerManagementCommands.LolwutAsync(CommandFlags)" path="/*[not(self::remarks) and not(self::returns)]" />
+    /// <returns>Command Response - <inheritdoc cref="IServerManagementCommands.LolwutAsync(CommandFlags)" /></returns>
     IBatch LolwutAsync();
 }
