ea46eff0384a6338e22f224e5a2a08c52582bf38
currantw <taylor.curran@improving.com>
2025-10-14 18:06:42 -0700

    Add new ClusterScanCursor class
    Signed-off-by: currantw <taylor.curran@improving.com>

---
+++ sources/Valkey.Glide/abstract_APITypes/ClusterScanCursor.cs
@@ -0,0 +1,44 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+namespace Valkey.Glide;
+
+/// <summary>
+/// A cursor used to iterate through data returned by cluster SCAN requests.
+/// </summary>
+public class ClusterScanCursor : IDisposable
+{
+    private const string FinishedCursorId = "finished";
+
+    /// <summary>
+    /// The cursor ID for this scan iteration.
+    /// </summary>
+    public string CursorId { get; internal set; }
+
+    /// <summary>
+    /// Indicates whether this cursor represents the end of the scan.
+    /// </summary>
+    public bool IsFinished => CursorId == FinishedCursorId;
+
+    /// <summary>
+    /// Creates a new ClusterScanCursor with the specified cursor ID.
+    /// </summary>
+    /// <param name="cursorId">The cursor ID.</param>
+    internal ClusterScanCursor(string cursorId)
+    {
+        CursorId = cursorId;
+    }
+
+    /// <summary>
+    /// Creates an initial cursor to start a new cluster scan.
+    /// </summary>
+    /// <returns>A new ClusterScanCursor for starting a scan.</returns>
+    public static ClusterScanCursor InitialCursor() => new("0");
+
+    /// <summary>
+    /// Releases resources associated with this cursor.
+    /// </summary>
+    public void Dispose()
+    {
+        // TODO: Release native cursor resources when FFI integration is added
+    }
+}
