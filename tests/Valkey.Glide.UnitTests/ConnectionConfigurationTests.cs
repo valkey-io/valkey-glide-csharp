34caecc2130d97c77e46c533daa2f34ae657faac
currantw <taylor.curran@improving.com>
2025-10-30 10:10:49 -0700

    Remove unnecessary `using` directive.
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.UnitTests/ConnectionConfigurationTests.cs
+++ tests/Valkey.Glide.UnitTests/ConnectionConfigurationTests.cs
@@ -1,7 +1,5 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
-using Valkey.Glide.Internals;
-
 using static Valkey.Glide.ConnectionConfiguration;
 
 namespace Valkey.Glide.UnitTests;
