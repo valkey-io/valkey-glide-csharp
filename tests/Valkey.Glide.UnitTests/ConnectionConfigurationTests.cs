6c940aa040fb1760c5c91f8acc9396441b171142
currantw <taylor.curran@improving.com>
2025-11-06 07:13:42 -0800

    Fix linting error
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.UnitTests/ConnectionConfigurationTests.cs
+++ tests/Valkey.Glide.UnitTests/ConnectionConfigurationTests.cs
@@ -1,5 +1,7 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
+using Valkey.Glide.Internals;
+
 using static Valkey.Glide.ConnectionConfiguration;
 
 namespace Valkey.Glide.UnitTests;
