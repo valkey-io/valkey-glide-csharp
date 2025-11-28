e16c13a2ed1b0dbb585bc86bef251ab6a5872ddb
currantw <taylor.curran@improving.com>
2025-11-28 09:03:23 -0800

    Add some newlines to `ConnectionRequest` to make it a bit more readable.
    Signed-off-by: currantw <taylor.curran@improving.com>

--- sources/Valkey.Glide/Internals/FFI.structs.cs
+++ sources/Valkey.Glide/Internals/FFI.structs.cs
@@ -953,39 +953,53 @@ internal partial class FFI
     {
         public nuint AddressCount;
         public IntPtr Addresses; // ** NodeAddress - array pointer
+
         [MarshalAs(UnmanagedType.U1)]
         public bool HasTlsMode;
         public TlsMode TlsMode;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool ClusterMode;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool HasRequestTimeout;
         public uint RequestTimeout;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool HasConnectionTimeout;
         public uint ConnectionTimeout;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool HasReadFrom;
         public ReadFrom ReadFrom;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool HasConnectionRetryStrategy;
         public RetryStrategy ConnectionRetryStrategy;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool HasAuthenticationInfo;
         public AuthenticationInfo AuthenticationInfo;
+
         public uint DatabaseId;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool HasProtocol;
         public ConnectionConfiguration.Protocol Protocol;
+
         [MarshalAs(UnmanagedType.LPStr)]
         public string? ClientName;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool LazyConnect;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool RefreshTopologyFromInitialNodes;
+
         [MarshalAs(UnmanagedType.U1)]
         public bool HasPubSubConfig;
         public PubSubConfigInfo PubSubConfig;
+
         // TODO more config params, see ffi.rs
     }
 
