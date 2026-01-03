1d61c2db8e0de2c1cf001325e6d9c9f30a59b3e2
currantw <taylor.curran@improving.com>
2026-01-02 16:27:16 -0800

    Update `CanConnectWithDifferentParameters` to use new config each time
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.IntegrationTests/StandaloneClientTests.cs
+++ tests/Valkey.Glide.IntegrationTests/StandaloneClientTests.cs
@@ -51,18 +51,16 @@ public class StandaloneClientTests(TestConfiguration config)
     [Fact]
     public async Task CanConnectWithDifferentParameters()
     {
-        var config = TestConfiguration.DefaultClientConfig();
-
-        await using var client1 = await GlideClient.CreateClient(config.WithClientName("GLIDE").Build());
-        await using var client2 = await GlideClient.CreateClient(config.WithTls(false).Build());
-        await using var client3 = await GlideClient.CreateClient(config.WithConnectionTimeout(TimeSpan.FromSeconds(2)).Build());
-        await using var client4 = await GlideClient.CreateClient(config.WithRequestTimeout(TimeSpan.FromSeconds(2)).Build());
-        await using var client5 = await GlideClient.CreateClient(config.WithDataBaseId(4).Build());
-        await using var client6 = await GlideClient.CreateClient(config.WithConnectionRetryStrategy(1, 2, 3).Build());
-        await using var client7 = await GlideClient.CreateClient(config.WithAuthentication("default", "").Build());
-        await using var client8 = await GlideClient.CreateClient(config.WithProtocolVersion(ConnectionConfiguration.Protocol.RESP2).Build());
-        await using var client9 = await GlideClient.CreateClient(config.WithReadFrom(new ConnectionConfiguration.ReadFrom(ConnectionConfiguration.ReadFromStrategy.Primary)).Build());
-        await using var client10 = await GlideClient.CreateClient(config.WithLazyConnect(true).Build());
+        await using var client1 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithClientName("GLIDE").Build());
+        await using var client2 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithTls(false).Build());
+        await using var client3 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithConnectionTimeout(TimeSpan.FromSeconds(2)).Build());
+        await using var client4 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithRequestTimeout(TimeSpan.FromSeconds(2)).Build());
+        await using var client5 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithDataBaseId(4).Build());
+        await using var client6 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithConnectionRetryStrategy(1, 2, 3).Build());
+        await using var client7 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithAuthentication("default", "").Build());
+        await using var client8 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithProtocolVersion(ConnectionConfiguration.Protocol.RESP2).Build());
+        await using var client9 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithReadFrom(new ConnectionConfiguration.ReadFrom(ConnectionConfiguration.ReadFromStrategy.Primary)).Build());
+        await using var client10 = await GlideClient.CreateClient(TestConfiguration.DefaultClientConfig().WithLazyConnect(true).Build());
     }
 
     [Theory(DisableDiscoveryEnumeration = true)]
@@ -509,7 +507,7 @@ public class StandaloneClientTests(TestConfiguration config)
 
         var initialCount = await GetConnectionCount(referenceClient);
 
-        // Create eager client (connects immediately).
+        `// Create eager client (connects immediately).
         await using var eagerClient = await GlideClient.CreateClient(eagerConfig);
 
         var connectCount = await GetConnectionCount(referenceClient);
