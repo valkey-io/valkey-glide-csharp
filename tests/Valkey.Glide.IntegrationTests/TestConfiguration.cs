77d79a4db5295a7f12b4a5e3e7bb57d744983a6c
currantw <taylor.curran@improving.com>
2026-01-19 09:51:50 -0800

    Rename `redis-server` to `valkey-server` and remove unnecessary workarounds
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.IntegrationTests/TestConfiguration.cs
+++ tests/Valkey.Glide.IntegrationTests/TestConfiguration.cs
@@ -12,8 +12,7 @@ namespace Valkey.Glide.IntegrationTests;
 public class TestConfiguration : IDisposable
 {
     // Default test timeout
-    // #184: Higher timeout needed on Windows due to WSL overhead.
-    public readonly static int DEFAULT_TIMEOUT_MS = OperatingSystem.IsWindows() ? 120_000 : 60_000;
+    public readonly static int DEFAULT_TIMEOUT_MS = 60_000; // 1 minute
     public readonly static TimeSpan DEFAULT_TIMEOUT = TimeSpan.FromMilliseconds(DEFAULT_TIMEOUT_MS);
 
     // Addresses for the standalone and cluster servers.
