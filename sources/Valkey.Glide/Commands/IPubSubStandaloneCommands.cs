41376db9c86911cc0812814dd6968ccdf08a8b6b
currantw <taylor.curran@improving.com>
2026-01-22 12:52:25 -0800

    Move `PublishAsync` to `IPubSubCommands`
    Signed-off-by: currantw <taylor.curran@improving.com>

--- sources/Valkey.Glide/Commands/IPubSubStandaloneCommands.cs
+++ sources/Valkey.Glide/Commands/IPubSubStandaloneCommands.cs
@@ -9,21 +9,5 @@ namespace Valkey.Glide.Commands;
 /// </summary>
 public interface IPubSubStandaloneCommands : IPubSubCommands
 {
-    /// <summary>
-    /// Publishes a message to the specified channel.
-    /// </summary>
-    /// <seealso href="https://valkey.io/commands/publish/">valkey.io</seealso>
-    /// <param name="channel">The channel to publish the message to.</param>
-    /// <param name="message">The message to publish.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns>The number of clients that received the message.</returns>
-    /// <remarks>
-    /// <example>
-    /// <code>
-    /// long subscriberCount = await client.PublishAsync("news", "Breaking news!");
-    /// Console.WriteLine($"Message delivered to {subscriberCount} subscribers");
-    /// </code>
-    /// </example>
-    /// </remarks>
-    Task<long> PublishAsync(string channel, string message, CommandFlags flags = CommandFlags.None);
+    // TODO #183: Add support for dynamic PubSub.
 }
