f045232366ca63b8258a17b57e04798ec153058a
currantw <taylor.curran@improving.com>
2026-02-23 10:38:39 -0800

    Increase max pub/sub duration slightly to avoid timeouts.
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.IntegrationTests/PubSubUtils.cs
+++ tests/Valkey.Glide.IntegrationTests/PubSubUtils.cs
@@ -174,7 +174,7 @@ public static class PubSubUtils
     /// <summary>
     /// Maximum duration for pub/sub assertions.
     /// </summary>
-    public static readonly TimeSpan MaxDuration = TimeSpan.FromSeconds(10);
+    public static readonly TimeSpan MaxDuration = TimeSpan.FromSeconds(12);
 
     /// <summary>
     /// Retry interval for pub/sub assertions.
