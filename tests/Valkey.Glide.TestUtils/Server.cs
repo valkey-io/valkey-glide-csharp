e4971570cc48d9b9a9474510d2ac408e26092d6f
currantw <taylor.curran@improving.com>
2026-03-11 09:52:29 -0700

    Add `SetPassword` and `ClearPassword` to `TestUtils.Server`.
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.TestUtils/Server.cs
+++ tests/Valkey.Glide.TestUtils/Server.cs
@@ -1,7 +1,5 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
-using System.Diagnostics;
-
 using static Valkey.Glide.ConnectionConfiguration;
 
 namespace Valkey.Glide.TestUtils;
@@ -11,25 +9,24 @@ namespace Valkey.Glide.TestUtils;
 /// </summary>
 public abstract class Server : IDisposable
 {
-    /// <summary>
-    /// Name of the server.
-    /// </summary>
-    private string _name = $"Server_{Guid.NewGuid():N}";
+    #region PrivateFields
 
-    /// <summary>
-    /// Indicates whether the server has been stopped.
-    /// </summary>
+    /// <summary>Name of the server.</summary>
+    private readonly string _name = $"Server_{Guid.NewGuid():N}";
+
+    #endregion
+    #region ProtectedFields
+
+    /// <summary>Indicates whether the server has been stopped.</summary>
     private bool _disposed = false;
 
-    /// <summary>
-    /// Indicates whether the server uses TLS.
-    /// </summary>
+    /// <summary>Indicates whether the server uses TLS.</summary>
     protected bool _useTls;
 
-    /// <summary>
-    /// Addresses of the server instances.
-    /// </summary>
+    /// <summary>Addresses of the server instances.</summary>
     protected IList<Address> _addresses;
+
+    #endregion
 
     protected Server(bool useClusterMode, bool useTls)
     {
@@ -42,7 +39,9 @@ public abstract class Server : IDisposable
     public void Dispose()
     {
         if (_disposed)
+        {
             return;
+        }
 
         _disposed = true;
         ServerManager.StopServer(_name);
@@ -53,25 +52,36 @@ public abstract class Server : IDisposable
     /// Builds and returns a client for this Valkey server.
     /// </summary>
     public abstract Task<BaseClient> CreateClient();
+
+    /// <summary>
+    /// Updates the password for the server to the given value.
+    /// </summary>
+    /// <param name="password">The new server password.</param>
+    public abstract Task SetPassword(string password);
+
+    /// <summary>
+    /// Clears the password for the server.
+    /// </summary>
+    public abstract Task ClearPassword();
 }
 
 /// <summary>
 /// Valkey cluster server.
 /// </summary>
-public sealed class ClusterServer : Server
+public sealed class ClusterServer(bool useTls = false) : Server(useClusterMode: true, useTls: useTls)
 {
-    public ClusterServer(bool useTls = false) : base(useClusterMode: true, useTls: useTls) { }
-
     /// <summary>
     /// Builds and returns a cluster client configuration builder for this Valkey server.
     /// </summary>
     public ClusterClientConfigurationBuilder CreateConfigBuilder()
     {
-        var configBuilder = new ClusterClientConfigurationBuilder();
-        configBuilder.WithTls(useTls: _useTls);
+        ClusterClientConfigurationBuilder configBuilder = new();
+        _ = configBuilder.WithTls(useTls: _useTls);
 
-        foreach (var (host, port) in _addresses)
-            configBuilder.WithAddress(host, port);
+        foreach ((string host, ushort port) in _addresses)
+        {
+            _ = configBuilder.WithAddress(host, port);
+        }
 
         return configBuilder;
     }
@@ -85,25 +95,38 @@ public sealed class ClusterServer : Server
     /// </summary>
     public async Task<GlideClusterClient> CreateClusterClient()
         => await GlideClusterClient.CreateClient(CreateConfigBuilder().Build());
+
+    public override async Task SetPassword(string password)
+    {
+        using GlideClusterClient client = await CreateClusterClient();
+        await client.ConfigSetAsync("requirepass", password, Route.AllNodes);
+    }
+
+    public override async Task ClearPassword()
+    {
+        using GlideClusterClient client = await CreateClusterClient();
+        await client.ConfigSetAsync("requirepass", "", Route.AllNodes);
+    }
 }
 
 /// <summary>
 /// Valkey standalone server.
 /// </summary>
-public sealed class StandaloneServer : Server
+public sealed class StandaloneServer(bool useTls = false) : Server(useClusterMode: false, useTls: useTls)
 {
-    public StandaloneServer(bool useTls = false) : base(useClusterMode: false, useTls: useTls) { }
 
     /// <summary>
     /// Builds and returns a standalone client configuration builder for this Valkey server.
     /// </summary>
     public StandaloneClientConfigurationBuilder CreateConfigBuilder()
     {
-        var configBuilder = new StandaloneClientConfigurationBuilder();
-        configBuilder.WithTls(useTls: _useTls);
+        StandaloneClientConfigurationBuilder configBuilder = new();
+        _ = configBuilder.WithTls(useTls: _useTls);
 
-        foreach (var (host, port) in _addresses)
-            configBuilder.WithAddress(host, port);
+        foreach ((string host, ushort port) in _addresses)
+        {
+            _ = configBuilder.WithAddress(host, port);
+        }
 
         return configBuilder;
     }
@@ -117,4 +140,16 @@ public sealed class StandaloneServer : Server
     /// </summary>
     public async Task<GlideClient> CreateStandaloneClient()
         => await GlideClient.CreateClient(CreateConfigBuilder().Build());
+
+    public override async Task SetPassword(string password)
+    {
+        using GlideClient client = await CreateStandaloneClient();
+        await client.ConfigSetAsync("requirepass", password);
+    }
+
+    public override async Task ClearPassword()
+    {
+        using GlideClient client = await CreateStandaloneClient();
+        await client.ConfigSetAsync("requirepass", "");
+    }
 }
