1bfb8c371bccef8f9966c729259c70934cc43791
currantw <taylor.curran@improving.com>
2026-03-19 13:58:44 -0700

    Fix failed batch tests
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.IntegrationTests/SharedBatchTests.cs
+++ tests/Valkey.Glide.IntegrationTests/SharedBatchTests.cs
@@ -79,11 +79,7 @@ public class SharedBatchTests
             ? (await ((GlideClusterClient)client).Exec((ClusterBatch)batch2, false))!
             : (await ((GlideClient)client).Exec((Batch)batch2, false))!;
 
-        Assert.Multiple(
-            () => Assert.Equal(1L, (long)res[0]!),
-            () => Assert.Null(res[1]),
-            () => Assert.Null(res[2])
-        );
+        Assert.Equivalent(new object[] { 1L, ValkeyValue.Null, ValkeyValue.Null }, res);
 
     }
 
