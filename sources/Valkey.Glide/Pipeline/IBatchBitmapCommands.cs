db930948fdda434b097a0046628d2160753438d6
Alex Rehnby-Martin <alex.rehnby-martin@improving.com>
2026-04-07 13:39:23 -0700

    refactor(bitmap): use GLIDE-style naming for batch bitmap commands
    - Rename batch methods from String* to GLIDE-style (GetBit, SetBit, etc.)
    - Use BitmapIndexType instead of StringIndexType in batch interface
    - Update BatchTestUtils to use new method names
    
    Signed-off-by: Alex Rehnby-Martin <alex.rehnby-martin@improving.com>

--- sources/Valkey.Glide/Pipeline/IBatchBitmapCommands.cs
+++ sources/Valkey.Glide/Pipeline/IBatchBitmapCommands.cs
@@ -14,7 +14,7 @@ internal interface IBatchBitmapCommands
     /// <param name="key">The key of the string.</param>
     /// <param name="offset">The offset in the string to get the bit at.</param>
     /// <returns>Command Response - The bit value stored at offset.</returns>
-    IBatch StringGetBit(ValkeyKey key, long offset);
+    IBatch GetBit(ValkeyKey key, long offset);
 
     /// <summary>
     /// Sets or clears the bit at offset in the string value stored at key.
@@ -24,7 +24,7 @@ internal interface IBatchBitmapCommands
     /// <param name="offset">The offset in the string to set the bit at.</param>
     /// <param name="value">The bit value to set (true for 1, false for 0).</param>
     /// <returns>Command Response - The original bit value stored at offset.</returns>
-    IBatch StringSetBit(ValkeyKey key, long offset, bool value);
+    IBatch SetBit(ValkeyKey key, long offset, bool value);
 
     /// <summary>
     /// Count the number of set bits in a string.
@@ -33,9 +33,9 @@ internal interface IBatchBitmapCommands
     /// <param name="key">The key of the string.</param>
     /// <param name="start">The start offset. Default is 0.</param>
     /// <param name="end">The end offset. Default is -1 (end of string).</param>
-    /// <param name="indexType">The index type (bit or byte). Default is <see cref="StringIndexType.Byte"/>.</param>
+    /// <param name="indexType">The index type (bit or byte). Default is <see cref="BitmapIndexType.Byte"/>.</param>
     /// <returns>Command Response - The number of bits set to 1.</returns>
-    IBatch StringBitCount(ValkeyKey key, long start = 0, long end = -1, StringIndexType indexType = StringIndexType.Byte);
+    IBatch BitCount(ValkeyKey key, long start = 0, long end = -1, BitmapIndexType indexType = BitmapIndexType.Byte);
 
     /// <summary>
     /// Return the position of the first bit set to 1 or 0 in a string.
@@ -45,9 +45,9 @@ internal interface IBatchBitmapCommands
     /// <param name="bit">The bit value to search for (true for 1, false for 0).</param>
     /// <param name="start">The start offset. Default is 0.</param>
     /// <param name="end">The end offset. Default is -1 (end of string).</param>
-    /// <param name="indexType">The index type (bit or byte). Default is <see cref="StringIndexType.Byte"/>.</param>
+    /// <param name="indexType">The index type (bit or byte). Default is <see cref="BitmapIndexType.Byte"/>.</param>
     /// <returns>Command Response - The position of the first bit with the specified value, or -1 if not found.</returns>
-    IBatch StringBitPosition(ValkeyKey key, bool bit, long start = 0, long end = -1, StringIndexType indexType = StringIndexType.Byte);
+    IBatch BitPos(ValkeyKey key, bool bit, long start = 0, long end = -1, BitmapIndexType indexType = BitmapIndexType.Byte);
 
     /// <summary>
     /// Perform a bitwise operation between two keys and store the result in the destination key.
@@ -58,7 +58,7 @@ internal interface IBatchBitmapCommands
     /// <param name="first">The first source key.</param>
     /// <param name="second">The second source key.</param>
     /// <returns>Command Response - The size of the string stored in the destination key.</returns>
-    IBatch StringBitOperation(Bitwise operation, ValkeyKey destination, ValkeyKey first, ValkeyKey second);
+    IBatch BitOp(Bitwise operation, ValkeyKey destination, ValkeyKey first, ValkeyKey second);
 
     /// <summary>
     /// Perform a bitwise operation between multiple keys and store the result in the destination key.
@@ -68,7 +68,7 @@ internal interface IBatchBitmapCommands
     /// <param name="destination">The key to store the result.</param>
     /// <param name="keys">The source keys.</param>
     /// <returns>Command Response - The size of the string stored in the destination key.</returns>
-    IBatch StringBitOperation(Bitwise operation, ValkeyKey destination, IEnumerable<ValkeyKey> keys);
+    IBatch BitOp(Bitwise operation, ValkeyKey destination, IEnumerable<ValkeyKey> keys);
 
     /// <summary>
     /// Reads or modifies the array of bits representing the string stored at key based on the specified subcommands.
@@ -77,7 +77,7 @@ internal interface IBatchBitmapCommands
     /// <param name="key">The key of the string.</param>
     /// <param name="subCommands">The subcommands to execute (GET, SET, INCRBY, OVERFLOW).</param>
     /// <returns>Command Response - An array of results from the executed subcommands.</returns>
-    IBatch StringBitField(ValkeyKey key, IEnumerable<Commands.Options.BitFieldOptions.IBitFieldSubCommand> subCommands);
+    IBatch BitField(ValkeyKey key, IEnumerable<Commands.Options.BitFieldOptions.IBitFieldSubCommand> subCommands);
 
     /// <summary>
     /// Reads the array of bits representing the string stored at key based on the specified GET subcommands.
@@ -87,5 +87,5 @@ internal interface IBatchBitmapCommands
     /// <param name="key">The key of the string.</param>
     /// <param name="subCommands">The GET subcommands to execute.</param>
     /// <returns>Command Response - An array of results from the executed GET subcommands.</returns>
-    IBatch StringBitFieldReadOnly(ValkeyKey key, IEnumerable<Commands.Options.BitFieldOptions.IBitFieldReadOnlySubCommand> subCommands);
+    IBatch BitFieldReadOnly(ValkeyKey key, IEnumerable<Commands.Options.BitFieldOptions.IBitFieldReadOnlySubCommand> subCommands);
 }
