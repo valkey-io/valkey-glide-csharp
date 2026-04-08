586c0f6dbdb2fc8b1997e2e23d8460a090135497
Alex Rehnby-Martin <alex.rehnby-martin@improving.com>
2026-04-08 09:42:18 -0700

    style(bitmap): remove unnecessary using directives
    Signed-off-by: Alex Rehnby-Martin <alex.rehnby-martin@improving.com>

--- sources/Valkey.Glide/Abstract/Database.BitmapCommands.cs
+++ sources/Valkey.Glide/Abstract/Database.BitmapCommands.cs
@@ -1,6 +1,5 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
-using Valkey.Glide.Commands.Options;
 using Valkey.Glide.Internals;
 
 namespace Valkey.Glide;
--- sources/Valkey.Glide/Abstract/IDatabaseAsync.BitmapCommands.cs
+++ sources/Valkey.Glide/Abstract/IDatabaseAsync.BitmapCommands.cs
@@ -1,7 +1,5 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
-using Valkey.Glide.Commands.Options;
-
 namespace Valkey.Glide;
 
 /// <summary>
