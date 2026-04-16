6469947acdb45e128694f73299df408c59b74e66
currantw <taylor.curran@improving.com>
2026-04-16 09:00:20 -0700

    Fix Options docstrings
    Signed-off-by: currantw <taylor.curran@improving.com>

--- sources/Valkey.Glide/Commands/Options/Options.cs
+++ sources/Valkey.Glide/Commands/Options/Options.cs
@@ -14,17 +14,17 @@ public abstract class Options
 
     /// <summary>
     /// Converts the given time span to milliseconds.
+    /// </summary>
     /// <param name="timeSpan">The time span to convert.</param>
     /// <returns>The milliseconds in the time span.</returns>
-    /// </summary>
     protected static GlideString ToMilliseconds(TimeSpan timeSpan)
         => (timeSpan.Ticks / TimeSpan.TicksPerMillisecond).ToGlideString();
 
     /// <summary>
     /// Converts the given date/time offset to UNIX milliseconds.
+    /// </summary>
     /// <param name="timestamp">The timestamp to convert.</param>
     /// <returns>The UNIX milliseconds for the timestamp.</returns>
-    /// </summary>
     protected static GlideString ToUnixMilliseconds(DateTimeOffset timestamp)
         => timestamp.ToUnixTimeMilliseconds().ToGlideString();
 }
