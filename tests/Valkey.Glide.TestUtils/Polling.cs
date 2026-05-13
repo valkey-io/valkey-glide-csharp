fce3d2e603ecb8a2b312154bc5308a0bfd27d8b6
currantw <taylor.curran@improving.com>
2026-05-13 12:38:17 -0700

    Add sync polling method
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.TestUtils/Polling.cs
+++ tests/Valkey.Glide.TestUtils/Polling.cs
@@ -28,6 +28,22 @@ public static class Polling
     /// Polls the given <paramref name="condition"/> until it returns <see langword="true"/>, or fails with
     /// <paramref name="failureMessage"/> if the <paramref name="timeout"/> expires.
     /// </summary>
+    /// <param name="condition">A function that returns <see langword="true"/> when the expected state is reached.</param>
+    /// <param name="failureMessage">The message to report if the condition is not met within the timeout.</param>
+    /// <param name="timeout">Maximum time to poll. Defaults to <see cref="DefaultTimeout"/>.</param>
+    /// <param name="interval">Delay between attempts. Defaults to <see cref="DefaultInterval"/>.</param>
+    public static Task AssertTrue(
+        Func<bool> condition,
+        string failureMessage,
+        TimeSpan? timeout = null,
+        TimeSpan? interval = null)
+        => AssertTrueAsync(()
+            => Task.FromResult(condition()), failureMessage, timeout, interval);
+
+    /// <summary>
+    /// Polls the given <paramref name="condition"/> until it returns <see langword="true"/>, or fails with
+    /// <paramref name="failureMessage"/> if the <paramref name="timeout"/> expires.
+    /// </summary>
     /// <param name="condition">An async function that returns <see langword="true"/> when the expected state is reached.</param>
     /// <param name="failureMessage">The message to report if the condition is not met within the timeout.</param>
     /// <param name="timeout">Maximum time to poll. Defaults to <see cref="DefaultTimeout"/>.</param>
