a6e5361072b65845f800f9ecc09f7418f16931d3
currantw <taylor.curran@improving.com>
2026-08-14 11:03:16 -0700

    Add StreamGroupOptions
    Signed-off-by: currantw <taylor.curran@improving.com>

---
+++ sources/Valkey.Glide/Commands/Options/StreamGroupOptions.cs
@@ -0,0 +1,24 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+namespace Valkey.Glide.Commands.Options;
+
+/// <summary>
+/// Optional arguments for the <c>XGROUP CREATE</c> command.
+/// </summary>
+/// <seealso href="https://valkey.io/commands/xgroup-create/"/>
+public sealed class StreamGroupOptions
+{
+    #region Public Properties
+
+    /// <summary>
+    /// Whether to create the stream if it does not already exist (MKSTREAM).
+    /// </summary>
+    public bool MakeStream { get; init; } = true;
+
+    /// <summary>
+    /// If specified, sets the group's entries-read counter to the given value (ENTRIESREAD).
+    /// </summary>
+    public long? EntriesRead { get; init; } = null;
+
+    #endregion
+}
