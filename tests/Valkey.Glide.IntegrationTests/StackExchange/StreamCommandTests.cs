9a27bfb39d5d2fed9f51485a7d6ad0c0e2d9916e
currantw <taylor.curran@improving.com>
2026-08-17 10:01:21 -0700

    Fix StreamAcknowledgeAsync_AcksMessage
    Signed-off-by: currantw <taylor.curran@improving.com>

--- tests/Valkey.Glide.IntegrationTests/StackExchange/StreamCommandTests.cs
+++ tests/Valkey.Glide.IntegrationTests/StackExchange/StreamCommandTests.cs
@@ -361,8 +361,7 @@ public class StreamCommandTests(TestConfiguration config)
         _ = await db.StreamCreateConsumerGroupAsync(key, "mygroup", "0");
         _ = await db.StreamReadGroupAsync(key, "mygroup", "consumer1", ">", count: 1);
 
-        long acked = await db.StreamAcknowledgeAsync(key, "mygroup", id1, CommandFlags.None);
-        Assert.Equal(1, acked);
+        Assert.True(await db.StreamAcknowledgeAsync(key, "mygroup", id1));
 
         StreamPendingInfo pending = await db.StreamPendingAsync(key, "mygroup");
         Assert.Equal(0, pending.PendingMessageCount);
